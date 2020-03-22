8e3382b5649b09868144d8bed61bcb97f4e1c23a
Kyle Partridge <partkyle@gmail.com>
2020-03-22 15:04:55 -0700

    initial window

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+extern crate piston_window;
+
+use piston_window::*;
+
+fn main() {
+    let window_size = [640, 480];
+    let mut window: PistonWindow = WindowSettings::new("Hello Piston!", window_size)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    while let Some(event) = window.next() {
+        window.draw_2d(&event, |context, graphics, _device| {
+            clear([1.0; 4], graphics);
+            rectangle(
+                [1.0, 0.0, 0.0, 1.0], // red
+                [0.0, 0.0, 100.0, 100.0],
+                context.transform,
+                graphics,
+            );
+        });
+    }
+}
